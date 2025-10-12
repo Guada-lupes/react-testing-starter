30c99124857254f51f0cbc971bf8ec5e19bdea8f
Guadalupe <gma.pedagoga@gmail.com>
2025-10-12 17:14:22 +0100

    configuraciones globales

--- test/components/Greet.test.tsx
+++ test/components/Greet.test.tsx
@@ -1,8 +1,6 @@
-import { it, expect, describe } from 'vitest'
-import {render, screen} from '@testing-library/react'
+
+import {render, screen} from  '@testing-library/react'
 import Greet from '../../src/components/Greet'
-
-
 
 //comenzamos importando las funciones de vitest
 describe('greet', () => {
@@ -14,6 +12,18 @@ describe('greet', () => {
         //utilizmos las queries de React para traer los componentes del DOM y guardamos en un const. En este caso usamos getByRole y'heading' nos traerá cualquier titulo
         const heading = screen.getByRole('heading');
         //debemos importa jest para comenzar a utilizar sus custom matchers
-        expect(heading).toBeInTheDocument()
+        expect(heading).toBeInTheDocument();
+        //utilizamos una expresión regular para el text. La i es para que no sea sensible a mayusculas
+        expect(heading).toHaveTextContent(/guada/i)
+    })
+        it('deberia renderizar Button cuando name no es dado', () => {
+        //renderizamos componente primero, para lo cual importamos desde react library
+        render(<Greet/>)
+        //utilizmos las queries de React para traer los componentes del DOM y guardamos en un const. En este caso usamos getByRole y'heading' nos traerá cualquier titulo
+        const button = screen.getByRole('button');
+        //debemos importa jest para comenzar a utilizar sus custom matchers
+        expect(button).toBeInTheDocument();
+        //utilizamos una expresión regular para el text. La i es para que no sea sensible a mayusculas
+        expect(button).toHaveTextContent(/login/i)
     })
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: { environment: "jsdom", setupFiles: ["./setupTest.ts"] },
+  //con globals evitamos tener que importa en cada archivo it, expect, describe. Pero debemos añadir "types": ["vitest/globals"] en tsconfig.json
+  test: { environment: "jsdom", setupFiles: ["test/setup.ts"], globals: true },
 });
